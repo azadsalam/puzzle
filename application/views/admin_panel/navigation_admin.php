fa48e9e557f782324c93295a05b401e388731da9
azadsalam <azadsalam2611@gmail.com>
2013-03-12 12:08:51 +0600

    naviagtion admin

--- application/views/admin_panel/navigation_admin.php
+++ application/views/admin_panel/navigation_admin.php
@@ -17,6 +17,10 @@
             <ul class="nav">
                 <li id="home_navbar"><a href="<?php echo site_url(); ?>">Main Picture Puzzle Website</a></li>
                 <li id="rules_navbar"><a href="<?php echo site_url('admin_panel/manage_puzzle'); ?>">Manage Puzzles</a></li>
+                                <li id=""><a href="<?php echo site_url('admin_panel/manage_user'); ?>">Manage User</a></li>
+                <li id=""><a href="<?php echo site_url('admin_panel/manage_progress'); ?>">Manage Progress</a></li>
+                <li id="rules_navbar"><a href="<?php echo site_url('admin_panel/manage_progress2'); ?>">Manage Progress with Name</a></li>
+
                 <li id="solve_navbar"><a href="<?php echo site_url('admin_panel/log'); ?>">View Log </a></li>
                 <li><a href="<?php  echo site_url('admin_panel/authentication/logout') ?>">Logout</a></li>
 
